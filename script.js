b00c42f495dd080c8a015c4bae157587eb5075a7
Patrick Ceriale <patceriale@gmail.com>
2020-09-20 18:29:45 -0700

    API not working...

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ let fiveDayForcast = $("five-day-container");
 
 //API call api.openweathermap.org/data/2.5/forecast?q={city name}&appid={b820eac750811c54acc2404d446a5629}
 // let queryURL: "https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={apiKey}";
+
 function displayWeather() {
   let city = $(this).attr("data-name");
   let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  // City Name (Date) cloud icon
+  // City Name (Date) *cloud icon
   //Temperature: in F
   //Humidity: 41%
   //Wind Speed: 4.7 MPH
@@ -21,17 +22,19 @@ function displayWeather() {
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    // $("#search-box").append(`<button class = "city-btn">${response.name}`);
     console.log(response); // TODO: remove console.log
     let title = $("<h4>").text(response.name);
     dailyForecast.append(title);
-    let temp = $("<p>").text(response.main.temp);
+    var tempF = (response.main.temp - 273.15) * 1.8 + 32; //Convert to degrees F
+    let temp = $("<p>").text(`Temperature: ${tempF} °F`);
     dailyForecast.append(temp);
-    let humidity = $("<p>").text(response.main.humidity);
+    let humidity = $("<p>").text(`Humidity: ${response.main.humidity}%`);
     dailyForecast.append(humidity);
-    let windSpeed = $("<p>").text(response.wind.speed);
+    let windSpeed = $("<p>").text(`Wind Speed: ${response.wind.speed} MPH`);
     dailyForecast.append(windSpeed);
-    // let uvIndex = $("<p>").text(response.main.temp);
+    // TODO: Find UV index
+    //let uvIndex = $("<p>").text(response.main.temp);
+    //uvIndex.addClass("uvIndex")
     // dailyForecast.append(uvIndex);
   });
 }
@@ -45,22 +48,30 @@ function displayWeather() {
 // }
 // TODO: create cityList array that adds a button to the searchBox after the user submits
 //Renders the city list buttons in the Search box
-function renderCities() {
+function renderCityBtn() {
   searchResults.empty();
   for (var i = 0; i < cityList.length; i++) {
     var createBtn = $("<button>");
-    createBtn.addClass("city-btn"); //TODO: Change class another exists in ajax request but might be fine?
+    createBtn.addClass("city-btn");
     createBtn.attr("data-name", cityList[i]);
-    searchResults.append(createBtn);
+    createBtn.text(cityList[i]);
+    searchResults.prepend(createBtn);
   }
 }
 
-// TODO: Handles events when a city button is clicked
+// TODO: Creates new city button
 $("#search-btn").on("click", function (event) {
   event.preventDefault();
   // Grab text from input box and add to cityList array
   let city = $("#city-input").val().trim();
   cityList.push(city);
-  renderCities();
+  if ((city = "")) {
+    // TODO: Stops function and doesn't add an empty box
+    return 0;
+  }
+  renderCityBtn();
 });
-renderCities();
+
+//CLick event listener toa ll elements with class of "city-btn"
+$(document).on("click", "city-btn", displayWeather);
+renderCityBtn();
